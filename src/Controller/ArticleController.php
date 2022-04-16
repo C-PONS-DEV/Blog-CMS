ea221e29f135eb6ecc27467bb2aad1e4b3f2c2c4
Clément PONS <cpons@MacBook-Pro-de-Clement.local>
2022-04-16 21:05:11 +0200

    Création du système d'articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
-    #[Route('/article/{slug', name: 'article_show')]
-    public function show(): Response
+    #[Route('/article/{slug}', name: 'article_show')]
+    public function show(?Article $article): Response
     {
+        if(!$article){
+            return $this->redirectToRoute('app_home');
+        }
         return $this->render('article/show.html.twig', [
-            'controller_name' => 'ArticleController',
+            'article' => $article,
         ]);
     }
 }
